13317b6d826311b1a9178a9999b0a52eb0a55743
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-07 06:45:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,4 +11,20 @@ class Show < ActiveRecord::Base
   def self.lowest_rating 
     self.minimum(:rating)
   end
+  
+  def self.least_popular_show
+    self.find_by(rating: lowest_rating)
+  end
+  
+  def self.rating_sum
+    self.sum(:rating)
+  end
+  
+  def popular_shows
+    self.where("rating > 5")
+  end
+  
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end 
 end
